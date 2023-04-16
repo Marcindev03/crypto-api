1b84e4ccaf87ac5d641f8d80f49bdbd39bf63838
Marcin Sacha <marcindev03@gmail.com>
2023-04-16 14:37:11 +0200

    Generate crud boilderplate for users currencies and transactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CurrenciesModule } from './currencies/currencies.module';
+import { UsersModule } from './users/users.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [CurrenciesModule, UsersModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
